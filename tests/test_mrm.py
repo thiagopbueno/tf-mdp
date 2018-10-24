5b0c54233f4427c8c8e97471191e38c995bb1cac
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-10-24 11:55:55 -0400

    Add initial_state() and __call__() methods to MRMCell class

--- tests/test_mrm.py
+++ tests/test_mrm.py
@@ -46,3 +46,157 @@ class TestMRMCell(unittest.TestCase):
         expected1 = (((2,),), ((2,),), ((2,), (2,)), 1, 1)
         actual1 = self.cell1.output_size
         self.assertTupleEqual(actual1, expected1)
+
+    def test_initial_state(self):
+        cells = [self.cell1]
+        batch_sizes = [self.batch_size1]
+        for cell, batch_size in zip(cells, batch_sizes):
+            initial_state = cell.initial_state()
+            self.assertIsInstance(initial_state, tuple)
+            self.assertEqual(len(initial_state), len(cell.state_size))
+            for t, shape in zip(initial_state, cell.state_size):
+                self.assertIsInstance(t, tf.Tensor)
+                expected_shape = [batch_size] + list(shape)
+                if len(expected_shape) == 1:
+                    expected_shape += [1]
+                self.assertListEqual(t.shape.as_list(), expected_shape)
+
+    def test_output_simulation_step(self):
+        horizon = 40
+        cells = [self.cell1]
+        batch_sizes = [self.batch_size1]
+        for cell, batch_size in zip(cells, batch_sizes):
+            with cell.graph.as_default():
+                # initial_state
+                initial_state = cell.initial_state()
+
+                # timestep
+                timestep = tf.constant(horizon, dtype=tf.float32)
+                timestep = tf.expand_dims(timestep, -1)
+                timestep = tf.stack([timestep] * batch_size)
+
+                # stop_flag
+                stop_flag = tf.constant(0.0, shape=(batch_size,1), dtype=tf.float32)
+
+                input = tf.concat([timestep, stop_flag], axis=1)
+
+                # simulation step
+                output, _ = cell(input, initial_state)
+                self.assertIsInstance(output, tuple)
+                self.assertEqual(len(output), 5)
+
+    def test_next_state_output(self):
+        horizon = 40
+        cells = [self.cell1]
+        batch_sizes = [self.batch_size1]
+        for cell, batch_size in zip(cells, batch_sizes):
+            with cell.graph.as_default():
+                # initial_state
+                initial_state = cell.initial_state()
+
+                # timestep
+                timestep = tf.constant(horizon, dtype=tf.float32)
+                timestep = tf.expand_dims(timestep, -1)
+                timestep = tf.stack([timestep] * batch_size)
+
+                # stop_flag
+                stop_flag = tf.constant(0.0, shape=(batch_size,1), dtype=tf.float32)
+
+                input = tf.concat([timestep, stop_flag], axis=1)
+
+                # simulation step
+                output, _ = cell(input, initial_state)
+                next_state, _, _, _, _ = output
+                next_state_size, _, _, _, _ = cell.output_size
+
+                self.assertIsInstance(next_state, tuple)
+                self.assertEqual(len(next_state), len(next_state_size))
+                for s, sz in zip(next_state, next_state_size):
+                    self.assertIsInstance(s, tf.Tensor)
+                    self.assertEqual(s.dtype, tf.float32)
+                    self.assertListEqual(s.shape.as_list(), [batch_size] + list(sz))
+
+    def test_action_output(self):
+        horizon = 40
+        cells = [self.cell1]
+        batch_sizes = [self.batch_size1]
+        for cell, batch_size in zip(cells, batch_sizes):
+            with cell.graph.as_default():
+                # initial_state
+                initial_state = cell.initial_state()
+
+                # timestep
+                timestep = tf.constant(horizon, dtype=tf.float32)
+                timestep = tf.expand_dims(timestep, -1)
+                timestep = tf.stack([timestep] * batch_size)
+
+                # stop_flag
+                stop_flag = tf.constant(0.0, shape=(batch_size,1), dtype=tf.float32)
+
+                input = tf.concat([timestep, stop_flag], axis=1)
+
+                # simulation step
+                output, _ = cell(input, initial_state)
+                _, action, _, _, _ = output
+                _, action_size, _, _, _ = cell.output_size
+
+                self.assertIsInstance(action, tuple)
+                self.assertEqual(len(action), len(action_size))
+                for a, sz in zip(action, action_size):
+                    self.assertIsInstance(a, tf.Tensor)
+                    self.assertEqual(a.dtype, tf.float32)
+                    self.assertListEqual(a.shape.as_list(), [batch_size] + list(sz))
+
+    def test_reward_output(self):
+        horizon = 40
+        cells = [self.cell1]
+        batch_sizes = [self.batch_size1]
+        for cell, batch_size in zip(cells, batch_sizes):
+            with cell.graph.as_default():
+                # initial_state
+                initial_state = cell.initial_state()
+
+                # timestep
+                timestep = tf.constant(horizon, dtype=tf.float32)
+                timestep = tf.expand_dims(timestep, -1)
+                timestep = tf.stack([timestep] * batch_size)
+
+                # stop_flag
+                stop_flag = tf.constant(0.0, shape=(batch_size,1), dtype=tf.float32)
+
+                input = tf.concat([timestep, stop_flag], axis=1)
+
+                # simulation step
+                output, _ = cell(input, initial_state)
+                _, _, _, reward, _ = output
+                _, _, _, reward_size, _ = cell.output_size
+
+                self.assertIsInstance(reward, tf.Tensor)
+                self.assertListEqual(reward.shape.as_list(), [batch_size, reward_size])
+
+    def test_log_prob_output(self):
+        horizon = 40
+        cells = [self.cell1]
+        batch_sizes = [self.batch_size1]
+        for cell, batch_size in zip(cells, batch_sizes):
+            with cell.graph.as_default():
+                # initial_state
+                initial_state = cell.initial_state()
+
+                # timestep
+                timestep = tf.constant(horizon, dtype=tf.float32)
+                timestep = tf.expand_dims(timestep, -1)
+                timestep = tf.stack([timestep] * batch_size)
+
+                # stop_flag
+                stop_flag = tf.constant(0.0, shape=(batch_size,1), dtype=tf.float32)
+
+                input = tf.concat([timestep, stop_flag], axis=1)
+
+                # simulation step
+                output, _ = cell(input, initial_state)
+                _, _, _, _, log_prob = output
+                _, _, _, _, log_prob_size = cell.output_size
+
+                self.assertIsInstance(log_prob, tf.Tensor)
+                self.assertListEqual(log_prob.shape.as_list(), [batch_size, log_prob_size])
--- tfmdp/train/mrm.py
+++ tfmdp/train/mrm.py
@@ -15,13 +15,15 @@
 
 
 from rddl2tf.compiler import Compiler
+from rddl2tf.fluent import TensorFluent
 from tfmdp.train.policy import DeepReactivePolicy
 
 import tensorflow as tf
 
-from typing import Iterable, Sequence, Optional, Tuple
+from typing import Iterable, Sequence, Optional, Tuple, Union
 
 Shape = Sequence[int]
+FluentTriple = Tuple[str, TensorFluent, TensorFluent]
 
 NonFluentsTensor = Sequence[tf.Tensor]
 StateTensor = Sequence[tf.Tensor]
@@ -38,6 +40,11 @@ class MRMCell(tf.nn.rnn_cell.RNNCell):
         self._compiler = compiler
         self._policy = policy
         self._batch_size = batch_size
+
+    @property
+    def graph(self) -> tf.Graph:
+        '''Returns the computation graph.'''
+        return self._compiler.graph
 
     @property
     def state_size(self) -> Sequence[Shape]:
@@ -59,8 +66,72 @@ class MRMCell(tf.nn.rnn_cell.RNNCell):
         '''Returns the simulation cell output size.'''
         return (self.state_size, self.action_size, self.interm_size, 1, 1)
 
+    def initial_state(self) -> StateTensor:
+        '''Returns the initial state tensor.'''
+        return self._compiler.compile_initial_state(self._batch_size)
+
     def __call__(self,
             input: tf.Tensor,
             state: Sequence[tf.Tensor],
             scope: Optional[str] = None) -> Tuple[CellOutput, CellState]:
-        pass
+
+        timestep, stop_flag = tf.split(input, [1, 1], axis=1)
+
+        # action
+        action = self._policy(state, input)
+
+        # next state
+        transition_scope = self._compiler.transition_scope(state, action)
+        interm_fluents, next_state_fluents = self._compiler.compile_probabilistic_cpfs(transition_scope, self._batch_size)
+
+        # log_probs
+        log_prob = self._log_prob(interm_fluents, next_state_fluents)
+
+        # reward
+        transition_scope.update({name: fluent for name, fluent, _ in next_state_fluents})
+        reward = self._compiler.compile_reward(transition_scope)
+        reward = reward.tensor
+
+        # outputs
+        interm_state = self._output(interm_fluents)
+        next_state = self._output(next_state_fluents)
+        output = (next_state, action, interm_state, reward, log_prob)
+
+        return (output, next_state)
+
+    def _log_prob(self, interm_fluents, next_state_fluents):
+
+        with self.graph.as_default():
+
+            interm_log_probs = [log_prob.tensor for _, _, log_prob in interm_fluents]
+            interm_log_prob = tf.reduce_sum(
+                tf.concat(interm_log_probs, axis=1, name='interm_log_probs'),
+                axis=1,
+                name='interm_log_prob')
+
+            next_state_log_probs = [log_prob.tensor for _, _, log_prob in next_state_fluents]
+            next_state_log_prob = tf.reduce_sum(
+                tf.concat(next_state_log_probs, axis=1, name='next_state_log_probs'),
+                axis=1,
+                name='next_state_log_prob')
+
+            log_prob = tf.expand_dims(interm_log_prob + next_state_log_prob, -1, name='log_prob')
+            return log_prob
+
+    @classmethod
+    def _tensors(cls, fluents: Sequence[FluentTriple]) -> Iterable[tf.Tensor]:
+        '''Yields the `fluents`' tensors.'''
+        for _, fluent, _ in fluents:
+            yield fluent.tensor
+
+    @classmethod
+    def _dtype(cls, tensor: tf.Tensor) -> tf.Tensor:
+        '''Converts `tensor` to tf.float32 datatype if needed.'''
+        if tensor.dtype != tf.float32:
+            tensor = tf.cast(tensor, tf.float32)
+        return tensor
+
+    @classmethod
+    def _output(cls, fluents: Sequence[FluentTriple]) -> Sequence[tf.Tensor]:
+        '''Returns output tensors for `fluents`.'''
+        return tuple(cls._dtype(t) for t in cls._tensors(fluents))
