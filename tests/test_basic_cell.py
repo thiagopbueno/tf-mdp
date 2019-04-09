55cb620aeb2bb5ed63c3880c77d42120b6e56a9d
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-04-09 12:10:20 -0400

    Add BasicMarkovCell class

---
+++ tests/test_basic_cell.py
@@ -0,0 +1,86 @@
+# This file is part of tf-mdp.
+
+# tf-mdp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-mdp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-mdp. If not, see <http://www.gnu.org/licenses/>.
+
+import rddlgym
+
+from tfmdp.policy.feedforward import FeedforwardPolicy
+from tfmdp.model.cell.basic_cell import BasicMarkovCell, OutputTuple, to_tensor
+
+import tensorflow as tf
+import unittest
+
+
+class TestBasicMarkovCell(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        # hyper-parameters
+        cls.horizon = 40
+        cls.batch_size = 16
+
+        # rddl
+        cls.compiler = rddlgym.make('Reservoir-8', mode=rddlgym.SCG)
+        cls.compiler.batch_mode_on()
+
+        # initial state
+        cls.initial_state = cls.compiler.compile_initial_state(cls.batch_size)
+
+        # default action
+        cls.default_action = cls.compiler.compile_default_action(cls.batch_size)
+
+        # policy
+        cls.policy = FeedforwardPolicy(cls.compiler, {'layers': [64, 64], 'activation': 'relu'})
+        cls.policy.build()
+
+        # cell
+        cls.cell = BasicMarkovCell(cls.compiler, cls.policy)
+
+        with cls.cell.graph.as_default():
+            # timestep
+            cls.timestep = tf.constant(cls.horizon, dtype=tf.float32)
+            cls.timestep = tf.expand_dims(cls.timestep, -1)
+            cls.timestep = tf.stack([cls.timestep] * cls.batch_size)
+
+    def test_state_size(self):
+        self.assertTupleEqual(self.cell.state_size, self.compiler.rddl.state_size)
+
+    def test_output_size(self):
+        state_size = self.cell.state_size
+        action_size = self.cell.action_size
+        interm_size = self.cell.interm_size
+        reward_size = 1
+        self.assertTupleEqual(self.cell.output_size, (state_size, action_size, interm_size, reward_size))
+
+    def test_call(self):
+        output, next_state = self.cell(self.timestep, self.initial_state)
+
+        self.assertIsInstance(output, OutputTuple)
+        self.assertEqual(len(output), 4)
+
+        self.assertEqual(output.state, output[0])
+        self.assertEqual(output.action, output[1])
+        self.assertEqual(output.interms, output[2])
+        self.assertEqual(output.reward, output[3])
+
+        self.assertEqual(output.state, next_state)
+
+        for action_fluent, default_action_fluent in zip(output.action, self.default_action):
+            self.assertEqual(action_fluent.shape, default_action_fluent.shape)
+
+        self.assertListEqual(output.reward.shape.as_list(), [self.batch_size, 1])
+
+        for fluent, next_fluent in zip(self.initial_state, next_state):
+            self.assertEqual(fluent.shape, next_fluent.shape)
---
+++ tfmdp/model/cell/basic_cell.py
@@ -0,0 +1,129 @@
+# This file is part of tf-mdp.
+
+# tf-mdp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-mdp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-mdp. If not, see <http://www.gnu.org/licenses/>.
+
+
+import rddl2tf
+
+from tfmdp.train.policy import DeepReactivePolicy
+
+import collections
+import tensorflow as tf
+
+from typing import Dict, Optional, Sequence, Tuple, Union
+
+Shape = Sequence[int]
+FluentPair = Tuple[str, rddl2tf.fluent.TensorFluent]
+
+NonFluentsTensor = Sequence[tf.Tensor]
+StateTensor = Sequence[tf.Tensor]
+StatesTensor = Sequence[tf.Tensor]
+ActionsTensor = Sequence[tf.Tensor]
+IntermsTensor = Sequence[tf.Tensor]
+
+CellOutput = Tuple[StatesTensor, ActionsTensor, IntermsTensor, tf.Tensor]
+CellState = Sequence[tf.Tensor]
+
+
+OutputTuple = collections.namedtuple('OutputTuple', 'state action interms reward')
+
+
+def cell_size(sizes: Sequence[Shape]) -> Sequence[Union[Shape, int]]:
+    return tuple(sz if sz != () else (1,) for sz in sizes)
+
+
+def to_tensor(fluents):
+    return tuple(f.tensor for f in fluents)
+
+
+class BasicMarkovCell(tf.nn.rnn_cell.RNNCell):
+    '''BasicMarkovCell implements a 1-step MDP transition function
+    as an RNNCell whose hidden state is the MDP current state and
+    output is a tuple with next state, action, intermediate fluents,
+    and reward.
+
+    Args:
+        compiler (:obj:`rddl2tf.compiler.Compiler`): RDDL2TensorFlow compiler.
+        config (Dict): The cell configuration parameters.
+    '''
+
+    def __init__(self,
+                 compiler: rddl2tf.compiler.Compiler,
+                 policy: DeepReactivePolicy,
+                 config: Optional[Dict] = None):
+        self.compiler = compiler
+        self.policy = policy
+        self.config = config
+
+    @property
+    def graph(self) -> tf.Graph:
+        '''Returns the cell's computation graph.'''
+        return self.compiler.graph
+
+    @property
+    def state_size(self) -> Sequence[Shape]:
+        '''Returns the MDP state size.'''
+        return cell_size(self.compiler.rddl.state_size)
+
+    @property
+    def action_size(self) -> Sequence[Shape]:
+        '''Returns the MDP action size.'''
+        return cell_size(self.compiler.rddl.action_size)
+
+    @property
+    def interm_size(self) -> Sequence[Shape]:
+        '''Returns the MDP intermediate state size.'''
+        return cell_size(self.compiler.rddl.interm_size)
+
+    @property
+    def output_size(self) -> Tuple[Sequence[Shape], Sequence[Shape], Sequence[Shape], int]:
+        '''Returns the simulation cell output size.'''
+        return (self.state_size, self.action_size, self.interm_size, 1)
+
+    def __call__(self,
+                 inputs: tf.Tensor,
+                 state: Sequence[tf.Tensor],
+                 scope: Optional[str] = None) -> Tuple[CellOutput, CellState]:
+        '''Returns the cell's output tuple and next state tensors.
+
+        Output tuple packs together the next state, action, interms,
+        and reward tensors in order.
+
+        Args:
+            inputs (tf.Tensor): The timestep input tensor.
+            state (Sequence[tf.Tensor]): The current state tensors.
+            scope (Optional[str]): The cell name scope.
+
+        Returns:
+            (CellOutput, CellState): A pair with the cell's output tuple and next state.
+        '''
+
+        # inputs
+        timestep = inputs
+
+        # action
+        action = self.policy(state, timestep)
+
+        # next state
+        interms, next_state = self.compiler.cpfs(state, action)
+
+        # reward
+        reward = self.compiler.reward(state, action, next_state)
+
+        # outputs
+        next_state = to_tensor(next_state)
+        interms = to_tensor(interms)
+        output = OutputTuple(next_state, action, interms, reward)
+
+        return (output, next_state)
