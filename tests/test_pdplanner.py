da703407b4bde6722c0e75340f9d636dd0fe0757
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-04-10 15:32:38 -0400

    Add PathwiseOptimizationPlanner class

---
+++ tests/test_pdplanner.py
@@ -0,0 +1,56 @@
+# This file is part of tf-mdp.
+
+# tf-mdp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-mdp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-mdp. If not, see <http://www.gnu.org/licenses/>.
+
+
+import rddlgym
+
+from tfmdp.policy.feedforward import FeedforwardPolicy
+from tfmdp.model.montecarlo import MonteCarloSampling
+from tfmdp.train.optimizers import optimizers
+from tfmdp.train.pdplanner import PathwiseOptimizationPlanner
+
+import unittest
+
+
+class TestPathwiseOptimizationPlanner(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        # hyper-parameters
+        cls.batch_size = 64
+        cls.horizon = 20
+        cls.learning_rate = 0.001
+
+        # rddl
+        cls.compiler = rddlgym.make('Reservoir-8', rddlgym.SCG)
+        cls.compiler.batch_mode_on()
+
+        # policy
+        cls.policy = FeedforwardPolicy(cls.compiler, {'layers': [512], 'activation': 'elu'})
+        cls.policy.build()
+
+        # planner
+        cls.config = {
+            'batch_size': cls.batch_size,
+            'horizon': cls.horizon,
+            'learning_rate': cls.learning_rate
+        }
+        cls.planner = PathwiseOptimizationPlanner(cls.compiler, cls.config)
+        cls.planner.build(cls.policy, loss='mse', optimizer='RMSProp')
+
+    def test_build(self):
+        self.assertIsInstance(self.planner.model, MonteCarloSampling)
+        self.assertIsInstance(self.planner.optimizer, optimizers['RMSProp'])
---
+++ tfmdp/train/pdplanner.py
@@ -0,0 +1,93 @@
+# This file is part of tf-mdp.
+
+# tf-mdp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-mdp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-mdp. If not, see <http://www.gnu.org/licenses/>.
+
+
+import rddl2tf.compiler
+
+from tfmdp.policy.drp import DeepReactivePolicy
+from tfmdp.model.montecarlo import MonteCarloSampling
+
+from  tfmdp.train.losses import loss_fn
+from tfmdp.train.optimizers import optimizers
+# from tfmdp.train.callbacks import Callback
+from tfmdp.train.planner import PolicyOptimizationPlanner
+
+import tensorflow as tf
+
+from typing import Callable, Dict, List, Optional, Sequence
+
+Callback = Callable[[None],None]
+Callbacks = Dict[str, Sequence[Callback]]
+
+
+class PathwiseOptimizationPlanner(PolicyOptimizationPlanner):
+    '''PathwiseOptimizationPlanner leverages pathwise derivative (PD)
+    gradient estimates to optimize the policy parameters for a given horizon,
+    batch size, and learning rate.
+
+    Args:
+        compiler (:obj:`rddl2tf.compiler.Compiler`): RDDL2TensorFlow compiler.
+        config (Dict): The planner configuration parameters.
+    '''
+
+    def __init__(self, compiler: rddl2tf.compiler.Compiler, config: Dict) -> None:
+        super(PathwiseOptimizationPlanner, self).__init__(compiler, config)
+
+        self.horizon = config['horizon']
+        self.batch_size = config['batch_size']
+        self.learning_rate = config['learning_rate']
+
+    def build(self, policy: DeepReactivePolicy,
+                    loss: str,
+                    optimizer: str) -> None:
+        '''Builds the PD planner for the given `loss` function and gradient `optimizer`.
+
+        Args:
+            policy (:obj:`tfmdp.policy.drp.DeepReactivePolicy`): A deep reactive policy.
+            loss (str): A differentiable loss function used to train the policy.
+            optimizer (str): A gradient descent optimizer.
+        '''
+        self.initial_state = self.compiler.compile_initial_state(self.batch_size)
+
+        with self.compiler.graph.as_default():
+
+            with tf.name_scope('model'):
+                self.model = MonteCarloSampling(self.compiler)
+                self.model.build(policy)
+                self.trajetory, self.final_state, self.total_reward = self.model(self.initial_state,
+                                                                                 self.horizon)
+
+            with tf.name_scope('loss'):
+                self.loss = loss_fn[loss](self.total_reward)
+
+            with tf.name_scope('optimizer'):
+                self.optimizer = optimizers[optimizer](self.learning_rate)
+                self.train_op = self.optimizer.minimize(self.loss)
+
+    def run(self, epochs: int, callbacks: Optional[Callbacks] = None) -> None:
+        '''Runs the policy optimizer for a given number of `epochs`.
+
+        Optionally, it executes `callbacks` to extend planning behavior
+        during training.
+
+        Args:
+            epochs (int): The number of training epochs.
+            callbacks (Optional[Dict[str, List[Callback]]]): Mapping from events to lists of callables.
+        '''
+        raise NotImplementedError
+
+    def summary(self) -> None:
+        '''Prints a string summary of the planner.'''
+        raise NotImplementedError
