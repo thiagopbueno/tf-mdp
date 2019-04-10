22818e9c312f0e318247cb8ac08c730ad278d6a4
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-04-10 11:47:28 -0400

    Add MonteCarloSampling recurrent model class

---
+++ tests/test_montecarlo.py
@@ -0,0 +1,83 @@
+# This file is part of tf-mdp.
+
+# tf-mdp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-mdp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-mdp. If not, see <http://www.gnu.org/licenses/>.
+
+import rddlgym
+
+from tfmdp.policy.feedforward import FeedforwardPolicy
+from tfmdp.model.cell.basic_cell import BasicMarkovCell
+from tfmdp.model.mrm import Trajectory
+from tfmdp.model.montecarlo import MonteCarloSampling
+
+import tensorflow as tf
+import unittest
+
+
+class TestMonteCarloSampling(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        # hyper-parameters
+        cls.horizon = 40
+        cls.batch_size = 128
+
+        # rddl
+        cls.compiler = rddlgym.make('Reservoir-8', rddlgym.SCG)
+        cls.compiler.batch_mode_on()
+
+        # initial state
+        cls.initial_state = cls.compiler.compile_initial_state(cls.batch_size)
+
+        # default action
+        cls.default_action = cls.compiler.compile_default_action(cls.batch_size)
+
+        # policy
+        cls.policy = FeedforwardPolicy(cls.compiler, {'layers': [32, 32], 'activation': 'elu'})
+        cls.policy.build()
+
+        # model
+        cls.config = {}
+        cls.model = MonteCarloSampling(cls.compiler, cls.config)
+        cls.model.build(cls.policy)
+
+    def test_build(self):
+        self.assertIsInstance(self.model.cell, BasicMarkovCell)
+        self.assertEqual(self.model.cell.policy, self.policy)
+
+    def test_call(self):
+        output = self.model(self.initial_state, self.horizon)
+        self.assertIsInstance(output, tuple)
+        self.assertEqual(len(output), 3)
+
+        trajectory, final_state, total_reward = output
+
+        self.assertIsInstance(trajectory, Trajectory)
+
+        for tensor, fluent in zip(trajectory.states, self.initial_state):
+            self.assertEqual(int(tensor.shape[0]), self.batch_size)
+            self.assertEqual(int(tensor.shape[1]), self.horizon)
+            self.assertListEqual(tensor.shape.as_list()[2:], fluent.shape.as_list()[1:])
+
+        for tensor, fluent in zip(trajectory.actions, self.default_action):
+            self.assertEqual(int(tensor.shape[0]), self.batch_size)
+            self.assertEqual(int(tensor.shape[1]), self.horizon)
+            self.assertListEqual(tensor.shape.as_list()[2:], fluent.shape.as_list()[1:])
+
+        self.assertIsInstance(final_state, tuple)
+        self.assertEqual(len(final_state), len(self.initial_state))
+
+        self.assertIsInstance(total_reward, tf.Tensor)
+        self.assertEqual(total_reward.dtype, tf.float32)
+        self.assertListEqual(total_reward.shape.as_list(), [self.batch_size])
---
+++ tfmdp/model/montecarlo.py
@@ -0,0 +1,88 @@
+# This file is part of tf-mdp.
+
+# tf-mdp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-mdp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-mdp. If not, see <http://www.gnu.org/licenses/>.
+
+
+import rddl2tf
+
+from tfmdp.policy.drp import DeepReactivePolicy
+from tfmdp.model.mrm import MarkovRecurrentModel, Trajectory
+from tfmdp.model.cell.basic_cell import BasicMarkovCell
+
+
+import tensorflow as tf
+from typing import Dict, Sequence, Optional, Tuple
+
+
+class MonteCarloSampling(MarkovRecurrentModel):
+    '''MonteCarloSampling class builds the symbolic computation graph
+    for approximating the finite-horizon expected return of a policy
+    for a given initial state by means of Monte-Carlo approximation.
+
+    Args:
+        compiler (:obj:`rddl2tf.compiler.Compiler`): RDDL2TensorFlow compiler.
+        config (Dict): The recurrent model configuration parameters.
+    '''
+
+    def __init__(self,
+                 compiler: rddl2tf.compiler.Compiler,
+                 config: Optional[Dict] = None) -> None:
+        super(MonteCarloSampling, self).__init__(compiler, config)
+
+    def build(self, policy: DeepReactivePolicy) -> None:
+        '''Builds a basic Markov cell ops by embedding the `policy` in the transition sampling.
+
+        Args:
+            policy (:obj:`tfmdp.policy.drp.DeepReactivePolicy`): A deep reactive policy.
+        '''
+        self.cell = BasicMarkovCell(self.compiler, policy)
+
+    def __call__(self,
+                 initial_state: Sequence[tf.Tensor],
+                 horizon: int) -> Tuple[Trajectory, Sequence[tf.Tensor], tf.Tensor]:
+        '''Samples a batch state-action-reward trajectory with given
+        `initial_state` and `horizon`, and returns the corresponding total reward.
+
+        Args:
+            initial_state (Sequence[tf.Tensor]): The initial state tensors.
+            horizon (int): The number of timesteps in each sampled trajectory.
+
+        Returns:
+            Tuple[Trajectory, Sequence[tf.Tensor], tf.Tensor]: A triple of (namedtuple, tensors, tensor)
+            representing the trajectory, final state, and total reward.
+        '''
+        with self.graph.as_default():
+
+            with tf.name_scope('trajectory'):
+                batch_size = int(initial_state[0].shape[0])
+                inputs = self.timesteps(horizon, batch_size)
+                outputs, final_state = tf.nn.dynamic_rnn(self.cell,
+                                                         inputs,
+                                                         initial_state=initial_state,
+                                                         dtype=tf.float32)
+
+                states = tuple(fluent[0] for fluent in outputs[0])
+                actions = tuple(fluent[0] for fluent in outputs[1])
+                interms = tuple(fluent[0] for fluent in outputs[2])
+                rewards = outputs[3]
+                trajectory = Trajectory(states, actions, interms, rewards)
+
+            with tf.name_scope('total_reward'):
+                total_reward = tf.reduce_sum(tf.squeeze(trajectory.rewards), axis=1)
+
+        return (trajectory, final_state, total_reward)
+
+    def summary(self) -> None:
+        '''Prints a string summary of the recurrent model.'''
+        raise NotImplementedError
