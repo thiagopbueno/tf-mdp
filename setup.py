421488b7d7b7d2df08224229507e61c778838924
Thiago P. Bueno <thiago.pbueno@gmail.com>
2021-06-23 23:08:45 -0300

    feat: install tf-mdp from PyPI in travis CI
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
-import tfmdp
+# This file is part of tf-plan.
+
+# tf-plan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-plan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-plan. If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=missing-docstring
 
 
 import os
 from setuptools import setup, find_packages
+
+import tfmdp
 
 
 def read(filename):
@@ -18,6 +35,7 @@ setup(
     author_email='thiago.pbueno@gmail.com',
     description='Probabilistic planning in continuous state-action MDPs using TensorFlow.',
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",
     license='GNU General Public License v3.0',
     keywords=['planning', 'tensorflow', 'rddl', 'mdp'],
     url='https://github.com/thiagopbueno/tf-mdp',
